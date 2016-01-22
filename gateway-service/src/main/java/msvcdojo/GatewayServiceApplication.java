f1b47f2242a3c8a63466adb9e23b95e182b8acfe
igormoochnick <igor@igorshare.com>
2016-01-22 00:27:21 -0500

    First pages commit

---
+++ gateway-service/src/main/java/msvcdojo/GatewayServiceApplication.java
@@ -0,0 +1,18 @@
+package msvcdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
