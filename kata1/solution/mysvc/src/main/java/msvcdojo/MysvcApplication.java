46b3a44247bcf0ec66f1ad538de4be692433be3f
igormoochnick <igor@igorshare.com>
2016-01-23 15:37:58 -0500

    Added guide to create simple web service

---
+++ kata1/solution/mysvc/src/main/java/msvcdojo/MysvcApplication.java
@@ -0,0 +1,14 @@
+package msvcdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// tag::code[]
+@SpringBootApplication
+public class MysvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysvcApplication.class, args);
+	}
+}
+// end::code[]
