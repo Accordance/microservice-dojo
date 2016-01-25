43eabe5348c3c7a4bbdc7baa7b170e14a3482187
igormoochnick <igor@igorshare.com>
2016-01-24 22:48:29 -0500

    Added a simple REST controller

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-cloud-kata
+
+Kata1 guide: http://accordance.github.io/microservice-dojo/guides/docs/kata1/creating_basic_web_service.html
--- kata1/solution/mysvc/src/main/java/msvcdojo/MysvcApplication.java
+++ kata1/solution/mysvc/src/main/java/msvcdojo/MysvcApplication.java
@@ -2,13 +2,26 @@ package msvcdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // tag::code[]
 @SpringBootApplication
 public class MysvcApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(MysvcApplication.class, args);
 	}
 }
 // end::code[]
+
+// tag::controller[]
+@RestController
+class HomeController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
+// end::controller[]
