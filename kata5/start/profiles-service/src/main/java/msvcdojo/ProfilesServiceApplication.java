25fc4378d4e239e0eaeab4807ebe1af17f875348
igormoochnick <igor@igorshare.com>
2016-02-04 13:12:18 -0500

    Added Kata5 start code

---
+++ kata5/start/profiles-service/src/main/java/msvcdojo/ProfilesServiceApplication.java
@@ -0,0 +1,16 @@
+package msvcdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Igor Moochnick
+ * Inspired by https://github.com/joshlong/spring-doge-microservice/tree/master/doge-service
+ */
+@SpringBootApplication
+public class ProfilesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilesServiceApplication.class, args);
+    }
+}
