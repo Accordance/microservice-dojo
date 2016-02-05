e1c24a459e58195070d05fb397e8e7528984e674
igormoochnick <igor@igorshare.com>
2016-02-05 00:36:08 -0500

    updated to Kata5 code

--- kata5/solution/profiles-service/src/main/java/msvcdojo/ProfilesServiceApplication.java
+++ kata5/solution/profiles-service/src/main/java/msvcdojo/ProfilesServiceApplication.java
@@ -3,15 +3,20 @@ package msvcdojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.AbstractResource;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -25,21 +30,42 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * @author Igor Moochnick
  * Inspired by https://github.com/joshlong/spring-doge-microservice/tree/master/doge-service
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class ProfilesServiceApplication {
 
+// tag::cleanup[]
     @Bean
-    CommandLineRunner init(ProfilePhotoRepository profilePhotoRepository) {
-        return a -> profilePhotoRepository.deleteAll();
+    CommandLineRunner init(ProfilesRepository profilesRepository) {
+        return a -> profilesRepository.deleteAll();
+    }
+// end::cleanup[]
+
+    @Bean
+    public ResourceProcessor<Resource<Profile>> personProcessor() {
+
+        return new ResourceProcessor<Resource<Profile>>() {
+
+            @Override
+            public Resource<Profile> process(Resource<Profile> resource) {
+
+                resource.add(linkTo(methodOn(ProfilePhotoController.class).readPhotos(resource.getContent().getKey())).withRel("photos"));
+
+                return resource;
+            }
+        };
     }
 
     public static void main(String[] args) {
@@ -48,11 +74,14 @@ public class ProfilesServiceApplication {
 }
 
 // tag::ProfilePhotoRepository[]
-interface ProfilePhotoRepository extends MongoRepository<ProfilePhoto, String> {
+@RepositoryRestResource
+interface ProfilesRepository extends MongoRepository<Profile, String> {
 
-    Collection<ProfilePhoto> findByKey(String key);
+    @Query("{ '_id' : ?0 }")
+    Profile findByKey(@Param("key") String key);
 
-    ProfilePhoto findByKeyAndId(String key, String id);
+    Profile findByFullName(@Param("address") String address);
+
 }
 // end::ProfilePhotoRepository[]
 
@@ -62,33 +91,36 @@ interface ProfilePhotoRepository extends MongoRepository<ProfilePhoto, String> {
 class ProfilePhotoController {
 // end::controller[]
 
-    private final ProfilePhotoRepository profilePhotoRepository;
+    private final ProfilesRepository profilesRepository;
     private final GridFsTemplate fs;
 
     @RequestMapping(method = RequestMethod.GET)
-    Collection<Resource<ProfilePhoto>> readPhotos(@PathVariable String key) {
+    Collection<Resource<String>> readPhotos(@PathVariable String key) {
 
-        Collection<Resource<ProfilePhoto>> coll = new LinkedList<>();
+        Profile profile = this.profilesRepository.findOne(key);
 
-        for (ProfilePhoto photo : this.profilePhotoRepository.findByKey(key)) {
-            Resource<ProfilePhoto> resource = new Resource<>(photo);
+        Collection<Resource<String>> coll = new LinkedList<>();
+
+        profile.photosList().forEach(photoId -> {
+            Resource<String> resource = new Resource<>(photoId);
             resource.add(new Link(ServletUriComponentsBuilder.fromCurrentRequest()
-                    .path("/{id}/photo").buildAndExpand(photo.getId()).toString(), "photo"));
+                    .path("/{id}/photo").buildAndExpand(photoId).toString(), photoId));
+
             coll.add(resource);
-        }
+        });
+
         return coll;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    Resource<ProfilePhoto> readPhoto(@PathVariable String key, @PathVariable String id) {
-        ProfilePhoto profilePhoto = this.profilePhotoRepository.findByKeyAndId(key, id);
-        return new Resource<ProfilePhoto>(profilePhoto);
+    Resource<Profile> readPhoto(@PathVariable String id) {
+        Profile profilePhoto = this.profilesRepository.findOne(id);
+        return new Resource<Profile>(profilePhoto);
     }
 
     @RequestMapping(value = "/{id}/photo", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    ResponseEntity<PhotoResource> readPhotoResource(@PathVariable String key, @PathVariable String id) {
-        ProfilePhoto profilePhoto = this.profilePhotoRepository.findByKeyAndId(key, id);
-        Photo photo = () -> this.fs.getResource(profilePhoto.getId()).getInputStream();
+    ResponseEntity<PhotoResource> readPhotoResource(@PathVariable String id) {
+        Photo photo = () -> this.fs.getResource(id).getInputStream();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.IMAGE_JPEG);
         return new ResponseEntity<>(new PhotoResource(photo), httpHeaders, HttpStatus.OK);
@@ -96,59 +128,76 @@ class ProfilePhotoController {
 
     // tag::insertPhoto[]
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
-    ResponseEntity<Resource<ProfilePhoto>> insertPhoto(@PathVariable String key,
+    ResponseEntity<Resource<Profile>> insertPhoto(@PathVariable String key,
                                                        @RequestParam MultipartFile file) throws IOException {
         Photo photo = file::getInputStream;
-        ProfilePhoto profilePhoto = this.profilePhotoRepository.save(new ProfilePhoto(key));
-        String id = profilePhoto.getId();
+        Profile profile = this.profilesRepository.findOne(key);
+        String id = key + profile.getPhotoCount();
         try (InputStream inputStream = photo.getInputStream()) {
             this.fs.store(inputStream, id);
         }
+        profile.addPhotoReference(id);
+        this.profilesRepository.save(profile);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}/photo").buildAndExpand(id).toUri();
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(uri);
         return new ResponseEntity<>(
-                this.readPhoto(key, id), headers, HttpStatus.CREATED);
+                this.readPhoto(key), headers, HttpStatus.CREATED);
     }
     // end::insertPhoto[]
 
     @Autowired
-    public ProfilePhotoController(ProfilePhotoRepository repository, GridFsTemplate gridFileSystem) {
-        this.profilePhotoRepository = repository;
+    public ProfilePhotoController(ProfilesRepository repository, GridFsTemplate gridFileSystem) {
+        this.profilesRepository = repository;
         this.fs = gridFileSystem;
     }
 
 }
 
 // tag::ProfilePhoto[]
-class ProfilePhoto {
-
-    public ProfilePhoto() {
-    }
-
-    public ProfilePhoto(String key) {
-        this.key = key;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
+@Document(collection="profiles")
+class Profile {
+    public Profile() {
     }
 
     @Id
     private String id;
-    private String key;
+    @Indexed
+    private String fullName;
+    private List<String> photos;
 
-    public String getId() {
-        return id;
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setKey(String key) {
+        this.id = key;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void addPhotoReference(String photoId) {
+        this.photosList().add(photoId);
     }
 
     public String getKey() {
-        return key;
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public Integer getPhotoCount() { return this.photosList().size(); }
+    public List<String> photosList() {
+        if (this.photos == null)
+            this.photos = new ArrayList<>();
+        return this.photos;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s, fullName='%s']",
+                id, fullName);
     }
 }
 // end::ProfilePhoto[]
